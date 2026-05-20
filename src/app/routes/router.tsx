2fdbcdc49b9b9607c1fc9b07c5372b82640bca4b
Juan Marin <marinjuanjose2005@gmail.com>
2026-05-19 22:25:06 -0500

    fix: import

--- src/app/routes/router.tsx
+++ src/app/routes/router.tsx
@@ -4,7 +4,8 @@ import { MainLayout } from "../components/MainLayout";
 import { homeRoutes } from "@/home/routes/home.routes";
 import { projectRoutes } from "@/proyects/routes/project.routes";
 import { aboutUsRoutes } from "@/about_us/routes/about_us.routes";
-import { tuVehiculoCheckRoutes } from "@/tu_vehiculo_check/routes/tu_vehiculo_check.routes";
+import { tuVehiculoCheckRoutes } from "tu_vehiculo_check/routes/tu_vehiculo_check.routes";
+
 
 export const routes = createBrowserRouter([
     {
